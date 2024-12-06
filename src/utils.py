3e28281dc7635af06467950d0b98780f8297f59a
Henri Lemoine <henri123lemoine@gmail.com>
2024-12-06 02:10:33 -0500

    added finding option in get_vocabulary

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,10 @@ from nltk.corpus import brown
 
 def get_vocabulary(top_n: int = 5000, min_word_len: int = 2) -> set[str]:
     """Get vocabulary from Brown corpus"""
-    nltk.download("brown", quiet=True)
+    try:
+        nltk.data.find("brown")
+    except LookupError:
+        nltk.download("brown", quiet=True)
     freq_dist = nltk.FreqDist(
         word.lower() for word in brown.words() if word.isalpha() and len(word) >= min_word_len
     )
